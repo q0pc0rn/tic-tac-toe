b766403e5e6ea165d501603f69990bec8301edfc
q0pc0rn <landogg@inbox.ru>
2017-09-23 07:55:34 +0300

    ggwp

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
 var x = 0;
 var o = 0;
-var winnnerX = 0;
+var winnerX = 0;
 var winnerO = 0;
-
+var winner = null;
+var top, left, right, bot, centr;
 function clickers(event) {
   let swipe = {
     target: event.target || event.srcElement
   };
-  let field;
   if (swipe.target.classList.length === 1) {
     if (x <= o) {
       swipe.target.classList.add("x");
       x++;
-      pushingX.push(swipe.target.id);
+      winnerX += +swipe.target.value;
     } else {
       swipe.target.classList.add("o");
       o++;
-      pushingO.push(swipe.target.id);
+      winnerO += +swipe.target.value;
     }
   };
-  console.log(swipe.target.id);
+  (o >= 3) ? ggwp() : null;
+  console.log(winner);
+}
+
+function ggwp() {
+  winner = (winnerO === 6) ? 'o winner' : (winnerX === 6) ? 'x winner' : null;
+  (winner != null) ? alert('gg wp '+ winner) : null;
+}
+
+
+
+
+
+function randomName() {
+  action: console.log('11');
+  delee: console.log('123123');
+  adding: console.log('ssd');
+}
+
+function qweasd(event) {
+  randomName(delee);
 }
